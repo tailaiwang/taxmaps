0ef41c5d84927e81b263b036089f7aa7baa22e76
Tailai Wang <tailaiwang01@gmail.com>
2020-03-28 17:40:15 -0400

    provincial finances

--- src/components/dashboard/personalbreakdown.component.js
+++ src/components/dashboard/personalbreakdown.component.js
@@ -4,6 +4,7 @@ import {Pie} from 'react-chartjs-2';
 
 var jsondata = require('./testing');
 var fedexpenses =require('./assets/json/federal-expenses');
+var provexpenses = require('./assets/json/provincial-expenses-on');
 var colours = require('./assets/colour-set');
 
 const getData = (jdata, colourset) => {
@@ -30,6 +31,30 @@ const getData = (jdata, colourset) => {
 	return data;
 }
 
+const getData2 = (jdata, colourset) =>{
+	var data = {
+		labels: [],
+		datasets: [{
+			data: [],
+			backgroundColor: [],
+			hoverBackgroundColor: []
+		}]
+	};
+	var i;
+	var end = jdata.length - 1;
+	var decimal;
+	for (i = 0; i < end; i++) {
+		data.labels.push(jdata[i].title);
+		console.log(jdata[i].title);
+		decimal = jdata[i].amount / jdata[end].amount;
+		data.datasets[0].data.push(decimal);
+		console.log(decimal);
+		data.datasets[0].backgroundColor.push(colourset[i]);
+		data.datasets[0].hoverBackgroundColor.push(colourset[i]);
+	}
+	return data;
+}
+
 
 
 class PersonalBreakdown extends Component {
@@ -40,6 +65,8 @@ class PersonalBreakdown extends Component {
             <div className="Chart">
               <h1>the chart goes here</h1>
               <Pie data={getData(fedexpenses, colours)} />
+			  <h1>the next chart goes here</h1>
+			  <Pie data={getData2(provexpenses, colours)}/>
             </div>
           </div>
         );
