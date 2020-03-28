85c9b77407374b6f4d85516f24d1763cf53e430a
indancisive <47584841+indancisive@users.noreply.github.com>
2020-03-28 15:34:26 -0400

    working on the graph

--- src/components/dashboard/personalbreakdown.component.js
+++ src/components/dashboard/personalbreakdown.component.js
@@ -2,26 +2,32 @@ import React, {Component} from 'react';
 import "../../App.css";
 import {Pie} from 'react-chartjs-2';
 
-const data = {
-	labels: [
-		'Red',
-		'Blue',
-		'Yellow'
-	],
-	datasets: [{
-		data: [300, 50, 100],
-		backgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		],
-		hoverBackgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		]
-	}]
-};
+var jsondata = require('./testing');
+var colours = require('./assets/colour-set')
+
+const getData = (jdata, colourset) => {
+	var data = {
+		labels: [],
+		datasets: [{
+			data: [],
+			backgroundColor: [],
+			hoverBackgroundColor: []
+		}]
+	};
+	var i;
+	console.log(jdata.length);
+	for (i = 0; i < jdata.length; i++) {
+		data.labels.push(jdata[i].title);
+		console.log(jdata[i].title);
+		data.datasets[0].data.push(jdata[i].decimal);
+		console.log(jdata[i].decimal);
+		data.datasets[0].backgroundColor.push(colourset[i]);
+		data.datasets[0].hoverBackgroundColor.push(colourset[i]);
+	}
+	return data;
+}
+
+
 
 class PersonalBreakdown extends Component {
 
@@ -30,7 +36,7 @@ class PersonalBreakdown extends Component {
             <div className="personalBreakdown">
             <div className="Chart">
               <h1>the chart goes here</h1>
-              <Pie data={data} />
+              <Pie data={getData(jsondata, colours)} />
             </div>
           </div>
         );
