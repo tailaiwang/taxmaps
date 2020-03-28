4e5500313a07814184b967c824fd6295bf225f46
Rahma Gillan <rahmagillan2001@gmail.com>
2020-03-28 15:30:24 -0400

    Form front-end

--- src/Components/taxInformationInput.js
+++ src/Components/taxInformationInput.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class taxInformationInput extends Component {
     constructor(props){
@@ -26,31 +28,43 @@ class taxInformationInput extends Component {
 
     render() {
         return (
-            
-            <form onSubmit={this.handleSubmit}>
-                <h1>Personal breakdown works</ h1>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                </label>
-                <label>
-                    Yearly Income:
-                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                </label>
-                <label>
-                    Yearly Consumption:
-                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                </label>
-                <label>
-                    Property Tax:
-                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                </label>
-                <label>
-                    Postal Code:
-                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                </label>
-                <input type="submit" value="Find My Tax Breakdowns"/>
-            </form>
+
+            <Form className="inputForm">
+              <Form.Group controlId="inputForm.name">
+                <Form.Label>Name</ Form.Label>
+                  <Form.Control type="" placeholder="John Doe" />
+              </ Form.Group>
+              <Form.Group controlId="inputForm.yearlyIncome">
+                <Form.Label>Yearly Income</ Form.Label>
+                  <Form.Control type="" placeholder="" />
+              </ Form.Group>
+              <Form.Group controlId="inputForm.yearlyConsumption">
+                <Form.Label>Yearly Consumpution</ Form.Label>
+                <Form.Control type="" placeholder="" />
+                <Form.Text className="text-muted">
+                  Enter an estimate of your annual spending
+                </ Form.Text>
+              </ Form.Group>
+              <Form.Group controlId="inputForm.checkPT">
+                <Form.Label>Do you own taxable property?</ Form.Label>
+                <Form.Check inline disables label="Yes" type="radio" id="yesPT"/>
+                <Form.Check inline disables label="No" type="radio" id="noPT"/>
+              </ Form.Group>
+              <Form.Group controlId="inputForm.propertyTax">
+                <Form.Label>Property Tax</ Form.Label>
+                <Form.Control type="" placeholder="" />
+              </ Form.Group>
+              <Form.Group controlId="inputForm.postalCode">
+                <Form.Label>Postal Code</ Form.Label>
+                <Form.Control type="" placeholder="" />
+              </ Form.Group>
+              <Button variant="success" type="submit">
+                Submit
+              </Button>
+            </ Form>
+
+
+
         );
     }
 }
