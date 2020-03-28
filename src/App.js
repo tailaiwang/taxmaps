ab34ed60c1dffd1740e22fd76e209baa8c770f5d
Rahma Gillan <rahmagillan2001@gmail.com>
2020-03-28 14:38:33 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AppRoute from './app.route';
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     render() {
