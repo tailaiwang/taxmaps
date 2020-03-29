02c5656ecd0d190fc1947b27276f911bced9bf45
Rahma Gillan <rahmagillan2001@gmail.com>
2020-03-29 06:15:22 -0400

    a homescreen as functionin as i am

--- src/Components/homeScreen.js
+++ src/Components/homeScreen.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router';
+import logo from './dashboard/assets/logos/coloured_logo_fitted.png';
+import {Link, Route} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import canmap from './dashboard/assets/can_map.svg';
 
 class homeScreen extends Component {
     render() {
         return (
-            <h1>this is the homescreen</h1>
+          <div>
+            <img id="homeLogo" src={logo} />
+            <img id="canmap" src={canmap} />
+            <Button className="learn-btn" variant="success" type="submit">
+              <Link className="btn-link" to="/taxInformationInput">Learn About My Taxes</Link>
+            </Button>
+            <Route path="/taxInformationInput">
+              <taxInformationInput />
+            </Route>
+          </div>
         );
     }
 }
