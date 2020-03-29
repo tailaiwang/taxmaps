87215ce184f2a01519aac9b828436c13bb814299
Tailai Wang <tailaiwang01@gmail.com>
2020-03-28 23:45:30 -0400

    Trudeau Done

--- src/components/dashboard/politicalcandidates.component.js
+++ src/components/dashboard/politicalcandidates.component.js
@@ -2,10 +2,50 @@ import React, {Component} from 'react'
 import "../../App.css";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import { Pie } from 'react-chartjs-2';
 
 var trudeaubudget =require('./assets/json/trudeau-budget');
 var trudeaupolicy = require('./assets/json/trudeau-policy-costs');
 var colours = require('./assets/colour-set');
+
+const getData = (jdata, colourset) =>{
+	var data = {
+		labels: [],
+		datasets: [{
+			data: [],
+			backgroundColor: [],
+			hoverBackgroundColor: []
+		}]
+	};
+	var i;
+	var end = jdata.length - 1;
+	var decimal;
+	for (i = 0; i < end; i++) {
+		data.labels.push(jdata[i].title);
+		decimal = jdata[i].amount / jdata[end].amount;
+		data.datasets[0].data.push(decimal);
+		data.datasets[0].backgroundColor.push(colourset[i]);
+		data.datasets[0].hoverBackgroundColor.push(colourset[i]);
+	}
+	return data;
+}
+
+const legendOpts = {
+  display: true,
+  position: 'right',
+  fullWidth: true,
+  fullHeight: true,
+  reverse: false,
+  boxWidth: 50,
+  align: 'start',
+  labels: {
+    fontColor: 'rgb(58, 62, 65)',
+    padding: 13,
+    fontSize: 14,
+    fontFamily: 'Raleway',
+    usePointStyle: false
+  }
+};
 
 class PoliticalCandidates extends Component {
     render() {
@@ -15,7 +55,10 @@ class PoliticalCandidates extends Component {
               <div>
               <Tabs defaultActiveKey="profile" id="tab-cands" className="poli-tabs">
                 <Tab eventKey="can1" title="Justin Trudeau">
-                  <p>I THINK HONG YI IS THE BEST CANDIDATE</p>
+                  <h4>Liberal Party Policy Implementation Costs (2019-2020)</h4>
+                  <Pie data={getData(trudeaupolicy, colours)} legend={legendOpts}/>
+                  <h4>Liberal Party Operating Budget (2019-2020)</h4>
+                  <Pie data={getData(trudeaubudget, colours)} legend={legendOpts}/>
                 </Tab>
                 <Tab eventKey="can2" title="Andrew Scheer">
                   <p>THE SECOND BEST IS TAILAI AND DANLU CUZ HES OKAY AT LOL</p>
