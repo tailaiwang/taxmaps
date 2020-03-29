4764a5bd41f8ffb1b47dcc279ca6baf3ac698f62
Tailai Wang <tailaiwang01@gmail.com>
2020-03-28 23:31:38 -0400

    Got started on poli

--- src/components/dashboard/politicalcandidates.component.js
+++ src/components/dashboard/politicalcandidates.component.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react'
+import "../../App.css";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+
+var trudeaubudget =require('./assets/json/trudeau-budget');
+var trudeaupolicy = require('./assets/json/trudeau-policy-costs');
+var colours = require('./assets/colour-set');
 
 class PoliticalCandidates extends Component {
     render() {
@@ -9,13 +14,13 @@ class PoliticalCandidates extends Component {
               <h1>Political Candidate Breakdowns</ h1>
               <div>
               <Tabs defaultActiveKey="profile" id="tab-cands" className="poli-tabs">
-                <Tab eventKey="can1" title="Candiadate A">
+                <Tab eventKey="can1" title="Justin Trudeau">
                   <p>I THINK HONG YI IS THE BEST CANDIDATE</p>
                 </Tab>
-                <Tab eventKey="can2" title="Candidate B">
+                <Tab eventKey="can2" title="Andrew Scheer">
                   <p>THE SECOND BEST IS TAILAI AND DANLU CUZ HES OKAY AT LOL</p>
                 </Tab>
-                <Tab eventKey="can3" title="Candidate C">
+                <Tab eventKey="can3" title="Jagmeet Singh">
                   <p>TIE FOR THIRD PLACE W UKNOW WHO (R GILLAN)</p>
                 </Tab>
                </Tabs>
