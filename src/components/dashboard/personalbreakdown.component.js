4355b733ade76870e018f0d2389ebd2ab38c5682
Hong Yi Chen <hongyigma@gmail.com>
2020-03-28 22:15:38 -0400

    do nut

--- src/components/dashboard/personalbreakdown.component.js
+++ src/components/dashboard/personalbreakdown.component.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {Pie} from 'react-chartjs-2';
 import "../../App.css";
+import { Doughnut } from 'react-chartjs-2';
 
 var jsondata = require('./testing');
 var fedexpenses =require('./assets/json/federal-expenses');
@@ -53,17 +53,36 @@ const getData2 = (jdata, colourset) =>{
 	return data;
 }
 
+const legendOpts = {
+  display: true,
+  position: 'right',
+  fullWidth: true,
+  fullHeight: true,
+  reverse: false,
+  boxWidth: 50,
+  labels: {
+    fontColor: 'rgb(58, 62, 65)',
+    padding: 13,
+    fontSize: 14,
+    fontFamily: 'Raleway'
+  }
+};
+
 class PersonalBreakdown extends Component {
     render() {
+
         return (
           <div className="personalBreakdown">
             <div className="chart">
               <h1 className="personalHeader">Personal Tax Breakdown</h1>
-              <Pie data={getData(fedexpenses, colours)} />
+              <Doughnut data={getData(fedexpenses, colours)} legend={legendOpts}/>
               </div>
+              
               <div className="chart">
               <h1 className="personalHeader">Tax Contributions and Spending</h1>
-              <Pie data={getData2(provexpenses, colours)}/>
+              <Doughnut 
+              data={getData2(provexpenses, colours)} 
+              legend={legendOpts}/>
             </div>
           </div>
         );
